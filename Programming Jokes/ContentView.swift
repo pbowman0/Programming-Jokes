d02107db728270fcb6244f62d677847d4ca0e69d
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-02-18 10:56:16 -0600

    got API key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -22,17 +22,12 @@ struct ContentView: View {
             .navigationTitle("Programming Jokes")
         }
         .onAppear(perform: {
-                    getJokes()
-                })
-
+            getJokes()
+        })
+        
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key = 0ff18549camsh01a37f90950c439p183210jsnf23412a0511c"
     }
 }
 
